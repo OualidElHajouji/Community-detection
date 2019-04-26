ac8f77bf02989b8abc86193d7a81ad0c86570370
Abdellah <abdellah.aznag@polytechnique.edu>
2019-04-26 17:08:47 +0200

    -Testing the clustering fiability over -First estimation of the foggy zone

--- test_cin_cout_hypothesis.py
+++ test_cin_cout_hypothesis.py
@@ -7,26 +7,36 @@ import numpy as np
 import spectral_clusering_1 as spc
 import black_stochastic_model as bsm
 import clustering_quality as qlt
+import matplotlib.pyplot as plt
 
-n = 100
+n = 150
 k = 2
 cin = 100
 cout = 10
 
+rmax = np.sqrt(cin/np.log(n))
+
 
 def misclustering_MonteCarloBasic(nb_X, nb_Simu, random = False):
-    Quality = np.array([[-1 for array in range(nb_X)]])
+    Quality = np.zeros((nb_Simu, nb_X))
     if(random):
         X = np.random.rand(nb_X)
     else:
-        X = np.array([i for i in range(nb_X+1)])/(nb_X+1)
+        U = np.linspace(0.3, 0.5, nb_X)
+        X = qlt.inverse(U, cin, n)
+        print(X)
     for j in range(nb_Simu):
-        row = np.array([])
-        for i in range(1, nb_X + 1):
+        for i in range(nb_X):
             A, classes = bsm.simulate(n, cin, X[i] * cin, k)
             clusters = spc.spectral_clustering(A, k)
-            row = np.append(row, qlt.normal(classes, clusters, n))
-        Quality = np.append(Quality, [row], 0)
-        print(row)
-    return Quality
+            Quality[j, i] = qlt.normal(classes, clusters, n)
+        print(Quality[j])
+    return U, np.mean(Quality, axis=0)
+
+X, Y = misclustering_MonteCarloBasic(40, 50)
+plt.plot(X, Y)
+
+plt.show()
+
+
 
