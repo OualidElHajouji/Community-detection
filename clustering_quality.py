d416d860cc28cd26d35f4014334790203d19fc8d
admin <oualid.el-hajouji@polytechnique.edu>
2019-04-26 15:52:29 +0200

    Added function inverse

--- clustering_quality.py
+++ clustering_quality.py
@@ -6,3 +6,11 @@ def normal(classes, clusters, size):
 
 def hyp_test(cin, cout, n):
     return np.sqrt(cin - cout)/np.log(n)
+
+def inverse(r,cin,n):
+    r1 = r*np.sqrt(np.log(n)/cin)
+    x = ((2+r1**2)-r1*np.sqrt(r1**2+8))/2
+    return x
+
+
+
